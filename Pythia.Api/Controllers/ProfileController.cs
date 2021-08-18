549ab3fdced34b01af246fb6fe6654d0bd0eb494
Myrmex <dfusi@hotmail.com>
2021-08-18 16:33:53 +0200

    Added profile controller

---
+++ Pythia.Api/Controllers/ProfileController.cs
@@ -0,0 +1,61 @@
+﻿using Corpus.Core;
+using Fusi.Tools.Data;
+using Microsoft.AspNetCore.Mvc;
+using Pythia.Api.Models;
+using Pythia.Core;
+using System;
+
+namespace Pythia.Api.Controllers
+{
+    /// <summary>
+    /// Indexing profiles.
+    /// </summary>
+    /// <seealso cref="ControllerBase" />
+    [ApiController]
+    public sealed class ProfileController : ControllerBase
+    {
+        private readonly IIndexRepository _repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileController"/>
+        /// class.
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        /// <exception cref="ArgumentNullException">repository</exception>
+        public ProfileController(IIndexRepository repository)
+        {
+            _repository = repository
+                ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        /// <summary>
+        /// Gets the specified page of profiles (without their content).
+        /// </summary>
+        /// <param name="model">The model. If you set the page size to 0,
+        /// all the profiles will be retrieved in a unique page.</param>
+        /// <returns>Profiles page.</returns>
+        [HttpGet("api/profiles")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<DataPage<Profile>> GetProfiles(
+            [FromQuery] ProfileFilterBindingModel model)
+        {
+            return Ok(_repository.GetProfiles(model.ToFilter()));
+        }
+
+        /// <summary>
+        /// Gets the profile with the specified ID.
+        /// </summary>
+        /// <param name="id">The profile identifier.</param>
+        /// <returns>The profile.</returns>
+        [HttpGet("api/profiles/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<Profile> GetProfile([FromRoute] string id)
+        {
+            Profile profile = _repository.GetProfile(id);
+            if (profile == null) return NotFound();
+            return Ok(profile);
+        }
+    }
+}
---
+++ Pythia.Api/Models/ProfileFilterBindingModel.cs
@@ -0,0 +1,63 @@
+﻿using Corpus.Core;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pythia.Api.Models
+{
+    /// <summary>
+    /// Profiles filter.
+    /// </summary>
+    public sealed class ProfileFilterBindingModel
+    {
+        /// <summary>
+        /// Page number (1-N).
+        /// </summary>
+        [Range(1, int.MaxValue)]
+        public int PageNumber { get; set; }
+
+        /// <summary>
+        /// Page size (0-100).
+        /// </summary>
+        [Range(0, 100)]
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// Any portion of the ID to be matched.
+        /// </summary>
+        [MaxLength(50)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The initial portion of the ID to be matched. Typically, profile
+        /// IDs begin with prefixes like <c>prefix-</c> when belonging to
+        /// different categories.
+        /// </summary>
+        [MaxLength(50)]
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileFilterBindingModel"/>
+        /// class.
+        /// </summary>
+        public ProfileFilterBindingModel()
+        {
+            PageNumber = 1;
+            PageSize = 20;
+        }
+
+        /// <summary>
+        /// Converts this model to the corresponding Pythia filter.
+        /// </summary>
+        /// <returns>Filter.</returns>
+        public ProfileFilter ToFilter()
+        {
+            return new ProfileFilter
+            {
+                PageNumber = PageNumber,
+                PageSize = PageSize,
+                Id = Id,
+                Prefix = Prefix
+            };
+        }
+    }
+}
