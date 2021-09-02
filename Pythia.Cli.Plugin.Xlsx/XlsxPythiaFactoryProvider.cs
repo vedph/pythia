839803849bcdf75c665ff49b85067b793d657964
Myrmex <dfusi@hotmail.com>
2021-09-02 11:14:56 +0200

    Added Excel attribute parser

---
+++ Pythia.Cli.Plugin.Xlsx/XlsxPythiaFactoryProvider.cs
@@ -0,0 +1,55 @@
+﻿using Corpus.Core.Plugin.Analysis;
+using Fusi.Tools.Config;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using Microsoft.Extensions.Configuration;
+using Pythia.Cli.Core;
+using Pythia.Core.Config;
+using Pythia.Core.Plugin.Analysis;
+using Pythia.Sql.PgSql;
+using Pythia.Xlsx.Plugin;
+using SimpleInjector;
+using System;
+
+namespace Pythia.Cli.Plugin.Xlsx
+{
+    /// <summary>
+    /// Excel Pythia factory provider. This adds Pythia.Xlsx.Plugin components
+    /// to the standard factory provider.
+    /// </summary>
+    /// <seealso cref="IPythiaFactoryProvider" />
+    [Tag("factory-provider.xlsx")]
+    public sealed class XlsxPythiaFactoryProvider : IPythiaFactoryProvider
+    {
+        public PythiaFactory GetFactory(string profileId, string profile,
+            string connString)
+        {
+            if (profileId == null)
+                throw new ArgumentNullException(nameof(profileId));
+            if (profile == null)
+                throw new ArgumentNullException(nameof(profile));
+            if (connString == null)
+                throw new ArgumentNullException(nameof(connString));
+
+            Container container = new Container();
+            PythiaFactory.ConfigureServices(container,
+                // Corpus.Core.Plugin
+                typeof(StandardDocSortKeyBuilder).Assembly,
+                // Pythia.Core.Plugin
+                typeof(StandardTokenizer).Assembly,
+                // Pythia.Xlsx.Plugin
+                typeof(FsExcelAttributeParser).Assembly,
+                // Pythia.Sql.PgSql
+                typeof(PgSqlTextRetriever).Assembly);
+            container.Verify();
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddInMemoryJson(profile);
+            IConfiguration configuration = builder.Build();
+
+            return new PythiaFactory(container, configuration)
+            {
+                ConnectionString = connString
+            };
+        }
+    }
+}
---
+++ Pythia.Xlsx.Plugin.Test/FsXlsxAttributeParserTest.cs
@@ -0,0 +1,78 @@
+using Corpus.Core;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Pythia.Xlsx.Plugin.Test
+{
+    public sealed class FsXlsxAttributeParserTest
+    {
+        [Fact]
+        public void Parse_Ok()
+        {
+            string filePath = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                @"..\..\..\Assets\Sample.xlsx"));
+
+            Document document = new Document
+            {
+                Source = filePath
+            };
+
+            FsExcelAttributeParser parser = new FsExcelAttributeParser();
+            parser.Configure(new FsXlsxAttributeParserOptions
+            {
+                NameColumnIndex = 0,
+                ValueColumnIndex = 1,
+                NameMappings = new[]
+                {
+                    "materia=subject",
+                    "sede di raccolta=coll-place",
+                    "organo giurisdizionale=",  // omit this attribute
+                    "sede organo giurisdizionale=court-place",
+                    "atto=act-type",
+                    "data=ymd",
+                    "grado#=degree"
+                },
+                ValueTrimming = true,
+                SheetIndex = 0
+            });
+
+            IList<Attribute> attrs =
+                parser.Parse(new StringReader("A fake document"), document);
+            Assert.Equal(6, attrs.Count);
+
+            var attr = attrs.FirstOrDefault(a => a.Name == "subject");
+            Assert.NotNull(attr);
+            Assert.Equal("civile", attr.Value);
+            Assert.Equal(AttributeType.Text, attr.Type);
+
+            attr = attrs.FirstOrDefault(a => a.Name == "coll-place");
+            Assert.NotNull(attr);
+            Assert.Equal("Lecce", attr.Value);
+            Assert.Equal(AttributeType.Text, attr.Type);
+
+            attr = attrs.FirstOrDefault(a => a.Name == "court-place");
+            Assert.NotNull(attr);
+            Assert.Equal("Taranto", attr.Value);
+            Assert.Equal(AttributeType.Text, attr.Type);
+
+            attr = attrs.FirstOrDefault(a => a.Name == "act-type");
+            Assert.NotNull(attr);
+            Assert.Equal("citazione", attr.Value);
+            Assert.Equal(AttributeType.Text, attr.Type);
+
+            attr = attrs.FirstOrDefault(a => a.Name == "ymd");
+            Assert.NotNull(attr);
+            Assert.Equal("20150418", attr.Value);
+            Assert.Equal(AttributeType.Text, attr.Type);
+
+            attr = attrs.FirstOrDefault(a => a.Name == "degree");
+            Assert.NotNull(attr);
+            Assert.Equal("2", attr.Value);
+            Assert.Equal(AttributeType.Number, attr.Type);
+        }
+    }
+}
