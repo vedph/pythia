e939280e74cb6ebe23f76ef8ff1c4feebb7eff41
Myrmex <dfusi@hotmail.com>
2022-05-04 19:09:51 +0200

    Updated packages and added CSV attr parser

--- Pythia.Core.Plugin/Analysis/FsCsvAttributeParser.cs
+++ Pythia.Core.Plugin/Analysis/FsCsvAttributeParser.cs
@@ -1,44 +1,34 @@
 ﻿using Corpus.Core;
 using Corpus.Core.Analysis;
 using Fusi.Tools.Config;
-using NPOI.HSSF.UserModel;
-using NPOI.SS.UserModel;
-using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
+using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
 
-namespace Pythia.Xlsx.Plugin
+namespace Pythia.Core.Plugin.Analysis
 {
     /// <summary>
-    /// File-system based Excel XLS/XLSX attribute parser. This parser assumes
-    /// that additional metadata for the document being parsed can be got from
-    /// an Excel file with a path which can be derived from the document's
-    /// source. It opens the file and reads metadata from 2 designed columns,
-    /// one representing names and the other representing values.
-    /// Additionally, it can rewrite the names as specified by its configuration
-    /// options.
-    /// The file format used is either the new Office Open XML format (XLSX),
-    /// or the legacy one (XLS), according to the extension. For any extension
-    /// different from <c>xls</c>, the new format is assumed.
-    /// <para>Tag: <c>attribute-parser.fs-excel</c>.</para>
+    /// CSV file-based attributes parser.
+    /// Tag: <c>attribute-parser.fs-csv</c>.
     /// </summary>
     /// <seealso cref="IAttributeParser" />
-    [Tag("attribute-parser.fs-excel")]
-    public sealed class FsExcelAttributeParser : IAttributeParser,
-        IConfigurable<FsXlsxAttributeParserOptions>
+    [Tag("attribute-parser.fs-csv")]
+    public sealed class FsCsvAttributeParser : IAttributeParser,
+        IConfigurable<FsCsvAttributeParserOptions>
     {
-        private FsXlsxAttributeParserOptions _options;
-        private readonly Dictionary<string, Tuple<string,AttributeType>> _mappings;
+        private readonly Dictionary<string, Tuple<string, AttributeType>> _mappings;
+        private FsCsvAttributeParserOptions _options;
         private Regex _findRegex;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FsExcelAttributeParser"/>
+        /// Initializes a new instance of the <see cref="FsCsvAttributeParser"/>
         /// class.
         /// </summary>
-        public FsExcelAttributeParser()
+        public FsCsvAttributeParser()
         {
             _mappings = new Dictionary<string, Tuple<string, AttributeType>>();
         }
@@ -48,7 +38,7 @@ namespace Pythia.Xlsx.Plugin
         /// </summary>
         /// <param name="options">The options.</param>
         /// <exception cref="ArgumentNullException">options</exception>
-        public void Configure(FsXlsxAttributeParserOptions options)
+        public void Configure(FsCsvAttributeParserOptions options)
         {
             _options = options ?? throw new ArgumentNullException(nameof(options));
 
@@ -78,24 +68,6 @@ namespace Pythia.Xlsx.Plugin
             }
         }
 
-        private static string GetCellValueAsString(IRow row, int index)
-        {
-            ICell cell = row.GetCell(index);
-            if (cell == null) return null;
-
-            switch (cell.CellType)
-            {
-                case CellType.Boolean:
-                    return cell.BooleanCellValue ? "1" : "0";
-                case CellType.Numeric:
-                    return cell.NumericCellValue.ToString(CultureInfo.InvariantCulture);
-                case CellType.String:
-                    return cell.StringCellValue;
-                default:
-                    return null;
-            }
-        }
-
         /// <summary>
         /// Parses the text from the specified reader.
         /// </summary>
@@ -113,48 +85,34 @@ namespace Pythia.Xlsx.Plugin
             if (document == null)
                 throw new ArgumentNullException(nameof(document));
 
-            // get the Excel file name
+            // get the CSV file name
             string filePath;
             if (_findRegex != null)
             {
-                filePath = _findRegex.Replace(document.Source, _options.SourceReplace);
+                filePath = _findRegex.Replace(document.Source,
+                    _options.SourceReplace);
                 if (filePath.Length == 0)
                     return Array.Empty<Corpus.Core.Attribute>();
             }
             else filePath = document.Source;
 
-            // open workbook
-            using FileStream file = new(filePath,
-                FileMode.Open, FileAccess.Read, FileShare.Read);
-            string ext = Path.GetExtension(filePath)?.ToLowerInvariant();
-            IWorkbook wbk = ext == ".xls"
-                ? new HSSFWorkbook(file)
-                : new XSSFWorkbook(file);
-
-            // read attributes
-            ISheet sheet = null;
+            // read file
             List<Corpus.Core.Attribute> attrs = new();
-
-            if (!string.IsNullOrEmpty(_options.SheetName))
-                sheet = wbk.GetSheet(_options.SheetName);
-
-            if (sheet == null)
-                sheet = wbk.GetSheetAt(_options.SheetIndex);
-
-            if (sheet == null) return attrs;
-
-            for (int i = 0; i <= sheet.LastRowNum; i++)
+            using StreamReader stream = new(new FileStream(filePath,
+                FileMode.Open, FileAccess.Read, FileShare.Read));
+            using var csv = new CsvReader(stream,
+                new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                IRow row = sheet.GetRow(i);
-                if (row != null)
+                Delimiter = _options.Separator ?? ",",
+                HasHeaderRecord = _options.HasHeader
+            });
+            while (csv.Read())
+            {
+                string name = csv.GetField(_options.NameColumnIndex);
+                string value = csv.GetField(_options.ValueColumnIndex);
+
+                if (!string.IsNullOrEmpty(name))
                 {
-                    string name = GetCellValueAsString(row,
-                        _options.NameColumnIndex);
-                    string value = GetCellValueAsString(row,
-                        _options.ValueColumnIndex);
-
-                    if (name == null || value == null) continue;
-
                     // trim name, and value if requested
                     name = name.Trim();
                     if (_options.ValueTrimming) value = value.Trim();
@@ -182,10 +140,21 @@ namespace Pythia.Xlsx.Plugin
     }
 
     /// <summary>
-    /// Options for <see cref="FsExcelAttributeParser"/>.
+    /// Options for <see cref="FsCsvAttributeParser"/>.
     /// </summary>
-    public class FsXlsxAttributeParserOptions
+    public class FsCsvAttributeParserOptions
     {
+        /// <summary>
+        /// Gets or sets the separator (default is comma).
+        /// </summary>
+        public string Separator { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the first record in the CSV
+        /// file corresponds to the header.
+        /// </summary>
+        public bool HasHeader { get; set; }
+
         /// <summary>
         /// Gets or sets the regular expression pattern to find in the source
         /// when replacing it with <see cref="SourceReplace"/>. If this is not
@@ -201,20 +170,6 @@ namespace Pythia.Xlsx.Plugin
         /// path can be built from it.
         /// </summary>
         public string SourceReplace { get; set; }
-
-        /// <summary>
-        /// Gets or sets the name of the sheet to load data from. You can set
-        /// either this one, or <see cref="SheetIndex"/>. If both are set,
-        /// <see cref="SheetName"/> has precedence.
-        /// </summary>
-        public string SheetName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the index of the sheet to load data from. You can set
-        /// either this one, or <see cref="SheetName"/>.  If both are set,
-        /// <see cref="SheetName"/> has precedence.
-        /// </summary>
-        public int SheetIndex { get; set; }
 
         /// <summary>
         /// Gets or sets the index of the name column in the Excel file.
