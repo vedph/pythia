3029ddc9f447f681d502d110f7ef695d843c4eb8
Myrmex <dfusi@hotmail.com>
2023-01-26 18:40:10 +0100

    Update build-cli.ps1

--- build-cli.ps1
+++ build-cli.ps1
@@ -2,12 +2,12 @@ cd .\pythia
 
 dotnet build -c Release
 dotnet publish -c Release
-compress-archive -path .\bin\Release\net7.0\publish\* -DestinationPath .\bin\Release\net7.0\pythia-any.zip -Force
+compress-archive -path .\bin\Release\net7.0\publish\* -DestinationPath .\bin\Release\pythia-any.zip -Force
 
 dotnet publish -c Release -r win-x64 --self-contained
 dotnet publish -c Release -r linux-x64 --self-contained
 dotnet publish -c Release -r osx-x64 --self-contained
 
-compress-archive -path .\bin\Release\net7.0\win-x64\publish\* -DestinationPath .\bin\Release\net7.0\win-x64\pythia-win-x64.zip -Force
-compress-archive -path .\bin\Release\net7.0\linux-x64\publish\* -DestinationPath .\bin\Release\net7.0\linux-x64\pythia-linux-x64.zip -Force
-compress-archive -path .\bin\Release\net7.0\osx-x64\publish\* -DestinationPath .\bin\Release\net7.0\osx-x64\pythia-osx-x64.zip -Force
+compress-archive -path .\bin\Release\net7.0\win-x64\publish\* -DestinationPath .\bin\Release\win-x64\pythia-win-x64.zip -Force
+compress-archive -path .\bin\Release\net7.0\linux-x64\publish\* -DestinationPath .\bin\Release\linux-x64\pythia-linux-x64.zip -Force
+compress-archive -path .\bin\Release\net7.0\osx-x64\publish\* -DestinationPath .\bin\Release\osx-x64\pythia-osx-x64.zip -Force
