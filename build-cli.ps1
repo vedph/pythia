bd04063c7057dbdf6a171f506007c93219893a7d
Myrmex <dfusi@hotmail.com>
2023-03-29 17:23:57 +0200

    Update build-cli.ps1

--- build-cli.ps1
+++ build-cli.ps1
@@ -8,6 +8,6 @@ dotnet publish -c Release -r win-x64 --self-contained
 dotnet publish -c Release -r linux-x64 --self-contained
 dotnet publish -c Release -r osx-x64 --self-contained
 
-compress-archive -path .\bin\Release\net7.0\win-x64\publish\* -DestinationPath .\bin\Release\win-x64\pythia-win-x64.zip -Force
-compress-archive -path .\bin\Release\net7.0\linux-x64\publish\* -DestinationPath .\bin\Release\linux-x64\pythia-linux-x64.zip -Force
-compress-archive -path .\bin\Release\net7.0\osx-x64\publish\* -DestinationPath .\bin\Release\osx-x64\pythia-osx-x64.zip -Force
+compress-archive -path .\bin\Release\net7.0\win-x64\publish\* -DestinationPath .\bin\Release\pythia-win-x64.zip -Force
+compress-archive -path .\bin\Release\net7.0\linux-x64\publish\* -DestinationPath .\bin\Release\pythia-linux-x64.zip -Force
+compress-archive -path .\bin\Release\net7.0\osx-x64\publish\* -DestinationPath .\bin\Release\pythia-osx-x64.zip -Force
