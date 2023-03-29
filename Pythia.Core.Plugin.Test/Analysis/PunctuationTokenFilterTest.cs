f8851914d8b15257097298830d13c1f2da14eebf
Myrmex <dfusi@hotmail.com>
2023-03-29 13:58:16 +0200

    Added PunctuationTokenFilter

---
+++ Pythia.Core.Plugin.Test/Analysis/PunctuationTokenFilterTest.cs
@@ -0,0 +1,158 @@
+﻿using Pythia.Core.Plugin.Analysis;
+using System.Linq;
+using Xunit;
+
+namespace Pythia.Core.Plugin.Test.Analysis;
+
+public sealed class PunctuationTokenFilterTest
+{
+    [Fact]
+    public void Apply_Empty_NoAttr()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 0,
+            Value = ""
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Empty(token.Attributes!);
+    }
+
+    [Fact]
+    public void Apply_NoPunct_NoAttr()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 3,
+            Value = "abc"
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Empty(token.Attributes!);
+    }
+
+    [Fact]
+    public void Apply_LeftPunct_Ok()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 4,
+            Value = "(abc"
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Single(token.Attributes!);
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "lp" && a.Value == "("));
+    }
+
+    [Fact]
+    public void Apply_LeftPuncts_Ok()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 5,
+            Value = "([abc"
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Single(token.Attributes!);
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "lp" && a.Value == "(["));
+    }
+
+    [Fact]
+    public void Apply_RightPunct_Ok()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 4,
+            Value = "abc."
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Single(token.Attributes!);
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "rp" && a.Value == "."));
+    }
+
+    [Fact]
+    public void Apply_RightPuncts_Ok()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 5,
+            Value = "abc?)"
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Single(token.Attributes!);
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "rp" && a.Value == "?)"));
+    }
+
+    [Fact]
+    public void Apply_LeftAndRightPuncts_Ok()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 6,
+            Value = "(abc!)"
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Equal(2, token.Attributes!.Count);
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "lp" && a.Value == "("));
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "rp" && a.Value == "!)"));
+    }
+
+    [Fact]
+    public void Apply_OnlyPuncts_Ok()
+    {
+        PunctuationTokenFilter filter = new();
+        Token token = new()
+        {
+            DocumentId = 1,
+            Position = 1,
+            Length = 5,
+            Value = "(...)"
+        };
+
+        filter.Apply(token, token.Position);
+
+        Assert.Single(token.Attributes!);
+        Assert.NotNull(token.Attributes!.FirstOrDefault(
+            a => a.Name == "lp" && a.Value == "(...)"));
+    }
+}
---
+++ Pythia.Core.Plugin/Analysis/PunctuationTokenFilter.cs
@@ -0,0 +1,49 @@
+﻿using Fusi.Tools;
+using Fusi.Tools.Configuration;
+using Pythia.Core.Analysis;
+using System;
+
+namespace Pythia.Core.Plugin.Analysis;
+
+/// <summary>
+/// A token filter which injects punctuation attributes for punctuation at
+/// the left/right (<c>lp</c> and <c>rp</c>) of a token. All the punctuation
+/// character(s) at the left up to the first non-punctuation character are
+/// considered as <c>lp</c>; all the punctuation character(s) at the right
+/// leftwards, up to the first non punctuation character and before <c>lp</c>
+/// characters are considered as <c>rp</c>.
+/// <para>Tag: <c>token-filter.punctuation</c>.</para>
+/// </summary>
+/// <seealso cref="ITokenFilter" />
+[Tag("token-filter.punctuation")]
+public sealed class PunctuationTokenFilter : ITokenFilter
+{
+    /// <summary>
+    /// Apply the filter to the specified token.
+    /// </summary>
+    /// <param name="token">The token.</param>
+    /// <param name="position">The position which will be assigned to
+    /// the resulting token, provided that it's not empty. Some filters
+    /// may use this value, e.g. to identify tokens like in deferred
+    /// POS tagging.</param>
+    /// <param name="context">The optional context.</param>
+    /// <exception cref="ArgumentNullException">token</exception>
+    public void Apply(Token token, int position, IHasDataDictionary? context = null)
+    {
+        if (token == null) throw new ArgumentNullException(nameof(token));
+
+        if (string.IsNullOrEmpty(token.Value)) return;
+
+        int a = 0;
+        while (a < token.Length && char.IsPunctuation(token.Value[a])) a++;
+
+        int b = token.Length - 1;
+        while (b > a && char.IsPunctuation(token.Value[b])) b--;
+
+        if (a > 0)
+            token.AddAttribute(new Corpus.Core.Attribute("lp", token.Value[..a]));
+
+        if (b < token.Length - 1)
+            token.AddAttribute(new Corpus.Core.Attribute("rp", token.Value[(b + 1)..]));
+    }
+}
