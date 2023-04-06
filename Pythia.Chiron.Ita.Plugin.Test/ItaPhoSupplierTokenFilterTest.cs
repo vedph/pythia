b675c703476e6732a979bd7f2ff14cb0ed27eeb4
Myrmex <dfusi@hotmail.com>
2023-04-06 08:39:31 +0200

    Update ItaPhoSupplierTokenFilterTest.cs

--- Pythia.Chiron.Ita.Plugin.Test/ItaPhoSupplierTokenFilterTest.cs
+++ Pythia.Chiron.Ita.Plugin.Test/ItaPhoSupplierTokenFilterTest.cs
@@ -8,7 +8,55 @@ namespace Pythia.Chiron.Ita.Plugin.Test;
 public sealed class ItaPhoSupplierTokenFilterTest
 {
     [Fact]
-    public void Apply_Default_Ok()
+    public void Apply_WithDigits_Nope()
+    {
+        ItaPhoSupplierTokenFilter filter = new();
+        Token token = new()
+        {
+            Length = 2,
+            Position = 1,
+            Value = "A4"
+        };
+
+        filter.Apply(token, 1);
+
+        Assert.Empty(token.Attributes!);
+    }
+
+    [Fact]
+    public void Apply_Email_Nope()
+    {
+        ItaPhoSupplierTokenFilter filter = new();
+        Token token = new()
+        {
+            Length = 17,
+            Position = 1,
+            Value = "abc@somewhere.com"
+        };
+
+        filter.Apply(token, 1);
+
+        Assert.Empty(token.Attributes!);
+    }
+
+    [Fact]
+    public void Apply_WithoutLetters_Nope()
+    {
+        ItaPhoSupplierTokenFilter filter = new();
+        Token token = new()
+        {
+            Length = 10,
+            Position = 1,
+            Value = "30/05/1970"
+        };
+
+        filter.Apply(token, 1);
+
+        Assert.Empty(token.Attributes!);
+    }
+
+    [Fact]
+    public void Apply_Word_Ok()
     {
         ItaPhoSupplierTokenFilter filter = new();
         Token token = new()
