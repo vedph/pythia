384c91b13114282ed6d917176cbe96ed81750212
Myrmex <dfusi@hotmail.com>
2024-08-06 12:53:46 +0200

    Update PushLocal.bat

--- PushLocal.bat
+++ PushLocal.bat
@@ -1,5 +1,5 @@
 @echo off
 echo PRESS ANY KEY TO INSTALL TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
-c:\exe\nuget add .\Pythia.Core\bin\Debug\Pythia.Core.4.0.0.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Core\bin\Debug\Pythia.Core.4.0.1.nupkg -source C:\Projects\_NuGet
 pause
