cc0eda60c1c439b2e5d1dff12ea3724725f00611
Myrmex <dfusi@hotmail.com>
2024-08-10 18:17:47 +0200

    Version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.0.2] - 2024-08-10
+
+- 2024-08-10: refactored locop translation.
 - 2024-08-09:
   - added search export endpoint.
   - refactored search endpoint as GET.
--- PushLocal.bat
+++ PushLocal.bat
@@ -1,9 +1,9 @@
 @echo off
 echo PRESS ANY KEY TO INSTALL TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
-c:\exe\nuget add .\Pythia.Core\bin\Debug\Pythia.Core.4.0.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Pythia.Cli.Core\bin\Debug\Pythia.Cli.Core.4.0.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Pythia.Core.Plugin\bin\Debug\Pythia.Core.Plugin.4.0.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Pythia.Sql\bin\Debug\Pythia.Sql.4.0.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Pythia.Sql.PgSql\bin\Debug\Pythia.Sql.PgSql.4.0.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Core\bin\Debug\Pythia.Core.4.0.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Cli.Core\bin\Debug\Pythia.Cli.Core.4.0.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Core.Plugin\bin\Debug\Pythia.Core.Plugin.4.0.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Sql\bin\Debug\Pythia.Sql.4.0.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Sql.PgSql\bin\Debug\Pythia.Sql.PgSql.4.0.2.nupkg -source C:\Projects\_NuGet
 pause
