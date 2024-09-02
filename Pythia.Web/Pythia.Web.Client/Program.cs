8d08b86811486c5ec6e229dc050e8ceba7724497
Myrmex <dfusi@hotmail.com>
2024-09-02 20:52:35 +0200

    Search

--- Pythia.Web/Pythia.Web.Client/Program.cs
+++ Pythia.Web/Pythia.Web.Client/Program.cs
@@ -8,7 +8,7 @@ internal static class Program
     static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        
+
         builder.Services.AddScoped<DialogService>();
         builder.Services.AddScoped<NotificationService>();
         builder.Services.AddScoped<TooltipService>();
