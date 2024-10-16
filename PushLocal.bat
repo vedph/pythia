de8484398c129e8f2417a06e1c83e88973d18575
Myrmex <dfusi@hotmail.com>
2024-10-16 11:38:19 +0200

    Updated auth packages

--- PushLocal.bat
+++ PushLocal.bat
@@ -2,8 +2,8 @@
 echo PRESS ANY KEY TO INSTALL TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 
-c:\exe\nuget add .\Pythia.Api.Models\bin\Debug\Pythia.Api.Models.4.1.2.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Pythia.Api.Services\bin\Debug\Pythia.Api.Services.4.1.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Api.Models\bin\Debug\Pythia.Api.Models.4.1.3.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Pythia.Api.Services\bin\Debug\Pythia.Api.Services.4.1.3.nupkg -source C:\Projects\_NuGet
 
 c:\exe\nuget add .\Pythia.Core\bin\Debug\Pythia.Core.4.1.1.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Pythia.Cli.Core\bin\Debug\Pythia.Cli.Core.4.1.1.nupkg -source C:\Projects\_NuGet
