3a8be19c935f613bf07d880c3af720b248b96e2b
Myrmex <dfusi@hotmail.com>
2024-12-04 19:34:38 +0100

    Auth claims

--- Pythia.Api.Controllers/AuthController.cs
+++ Pythia.Api.Controllers/AuthController.cs
@@ -3,6 +3,9 @@ using Fusi.Api.Auth.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Pythia.Api.Controllers;
 
@@ -24,4 +27,24 @@ public sealed class AuthController(UserManager<NamedUser> userManager,
     AuthControllerBase<NamedUser, IdentityRole>(
         userManager, roleManager, signInManager, configuration)
 {
+    protected override async Task<IList<Claim>> GetUserClaimsAsync(NamedUser user)
+    {
+        IList<Claim> claims = await base.GetUserClaimsAsync(user);
+
+        // claims from additional user properties
+        // http://docs.oasis-open.org/imi/identity/v1.0/os/identity-1.0-spec-os.html#_Toc229451870
+
+        if (!string.IsNullOrEmpty(user.FirstName))
+        {
+            claims.Add(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname",
+                user.FirstName));
+        }
+        if (!string.IsNullOrEmpty(user.LastName))
+        {
+            claims.Add(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname",
+                user.LastName));
+        }
+
+        return claims;
+    }
 }
