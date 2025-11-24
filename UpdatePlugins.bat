0817c0f7ea1db42d612e493d840a46dd6bc3e861
Myrmex <dfusi@hotmail.com>
2025-11-24 12:24:09 +0100

    Update UpdatePlugins.bat

--- UpdatePlugins.bat
+++ UpdatePlugins.bat
@@ -1,16 +1,16 @@
 @echo off
 echo COPY PLUGINS TO CLI
 echo DEBUG
-xcopy .\Pythia.Cli.Plugin.Chiron\bin\Debug\net9.0\ .\pythia\bin\Debug\net9.0\plugins\Pythia.Cli.Plugin.Chiron\ /y
-xcopy .\Pythia.Cli.Plugin.Standard\bin\Debug\net9.0\ .\pythia\bin\Debug\net9.0\plugins\Pythia.Cli.Plugin.Standard\ /y
-xcopy .\Pythia.Cli.Plugin.Udp\bin\Debug\net9.0\ .\pythia\bin\Debug\net9.0\plugins\Pythia.Cli.Plugin.Udp\ /y
-xcopy .\Pythia.Cli.Plugin.Xlsx\bin\Debug\net9.0\ .\pythia\bin\Debug\net9.0\plugins\Pythia.Cli.Plugin.Xlsx\ /y
+REM xcopy .\Pythia.Cli.Plugin.Chiron\bin\Debug\net10.0\ .\pythia\bin\Debug\net10.0\plugins\Pythia.Cli.Plugin.Chiron\ /y
+xcopy .\Pythia.Cli.Plugin.Standard\bin\Debug\net10.0\ .\pythia\bin\Debug\net10.0\plugins\Pythia.Cli.Plugin.Standard\ /y
+xcopy .\Pythia.Cli.Plugin.Udp\bin\Debug\net10.0\ .\pythia\bin\Debug\net10.0\plugins\Pythia.Cli.Plugin.Udp\ /y
+xcopy .\Pythia.Cli.Plugin.Xlsx\bin\Debug\net10.0\ .\pythia\bin\Debug\net10.0\plugins\Pythia.Cli.Plugin.Xlsx\ /y
 pause
 
 echo RELEASE
-xcopy .\Pythia.Cli.Plugin.Chiron\bin\Release\net9.0\ .\pythia\bin\Release\net9.0\plugins\Pythia.Cli.Plugin.Chiron\ /y
-xcopy .\Pythia.Cli.Plugin.Standard\bin\Release\net9.0\ .\pythia\bin\Release\net9.0\plugins\Pythia.Cli.Plugin.Standard\ /y
-xcopy .\Pythia.Cli.Plugin.Udp\bin\Release\net9.0\ .\pythia\bin\Release\net9.0\plugins\Pythia.Cli.Plugin.Udp\ /y
-xcopy .\Pythia.Cli.Plugin.Xlsx\bin\Release\net9.0\ .\pythia\bin\Release\net9.0\plugins\Pythia.Cli.Plugin.Xlsx\ /y
+REM xcopy .\Pythia.Cli.Plugin.Chiron\bin\Release\net10.0\ .\pythia\bin\Release\net10.0\plugins\Pythia.Cli.Plugin.Chiron\ /y
+xcopy .\Pythia.Cli.Plugin.Standard\bin\Release\net10.0\ .\pythia\bin\Release\net10.0\plugins\Pythia.Cli.Plugin.Standard\ /y
+xcopy .\Pythia.Cli.Plugin.Udp\bin\Release\net10.0\ .\pythia\bin\Release\net10.0\plugins\Pythia.Cli.Plugin.Udp\ /y
+xcopy .\Pythia.Cli.Plugin.Xlsx\bin\Release\net10.0\ .\pythia\bin\Release\net10.0\plugins\Pythia.Cli.Plugin.Xlsx\ /y
 
 pause
